0209b0d1e3419e0c5cd064a7372bda4ea75cacd6
XakedowX <thecode113@gmail.com>
2021-07-06 19:28:27 +0400

    made my own function + made power function myself

--- learn.cpp
+++ learn.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double power(double base, int exponent)
+{
+    double result = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        result = result * base;
+    }
+
+    return result;
+}
+
+
+void myMessageForYouGuys();
+
 
 int main()
 {
-    int battery = 67;
+    int base, exponent;
+    
+    cout << "What is the base?: ";
+    cin >> base;
+    cout << "What is the exponent?: ";
+    cin >> exponent;
 
-    int newBattery;
-    newBattery = 66;
-    newBattery = newBattery - 1;
+    cout << power(base, exponent) << endl;
 
-    std::cout << "Battery: " << newBattery << std::endl;
+    myMessageForYouGuys();
+}
 
-    int age;
-    std::cout << "\nWhat is your age?: ";
-    std::cin >> age;
 
-    std::cout << "Your age is: " << age << std::endl;
-
-    return 0;
+void myMessageForYouGuys()
+{
+    cout << "Why are you wasting your time on me? I mean thanks but...Are you here to see my progress?" << endl;
 }
